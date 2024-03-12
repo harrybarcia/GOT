f5fd548d102c582b47432203857c05438824aebd
harrybarcia <barcia.harry@gmail.com>
2024-03-11 21:23:23 -0700

    Leaflet map

--- app/main.js
+++ app/main.js
@@ -1,13 +1,21 @@
 import './main.scss'
 import template from './main.html'
-
+import { Map } from './components/map/map'
 import { InfoPanel } from './components/info-panel/info-panel'
 
 /** Main UI Controller Class */
 class ViewController {
-  /** Initialize Application */
-  constructor () {
+   /** Initialize Application */
+   constructor () {
     document.getElementById('app').outerHTML = template
+
+    // Initialize API service
+    if (window.location.hostname === 'localhost') {
+      this.api = new ApiService('http://localhost:5000/')
+    } else {
+      this.api = new ApiService('https://api.atlasofthrones.com/')
+    }
+
     this.initializeComponents()
   }
 
@@ -15,6 +23,8 @@ class ViewController {
   initializeComponents () {
     // Initialize Info Panel
     this.infoComponent = new InfoPanel('info-panel-placeholder')
+    this.mapComponent = new Map('map-placeholder')
+
   }
 }
 
