31771fa24e72bafc0667ed28b584dcbba1110673
harrybarcia <barcia.harry@gmail.com>
2024-03-11 21:00:31 -0700

    starting the front end, nothing selected toggle

---
+++ app/main.js
@@ -0,0 +1,21 @@
+import './main.scss'
+import template from './main.html'
+
+import { InfoPanel } from './components/info-panel/info-panel'
+
+/** Main UI Controller Class */
+class ViewController {
+  /** Initialize Application */
+  constructor () {
+    document.getElementById('app').outerHTML = template
+    this.initializeComponents()
+  }
+
+  /** Initialize Components with data and event listeners */
+  initializeComponents () {
+    // Initialize Info Panel
+    this.infoComponent = new InfoPanel('info-panel-placeholder')
+  }
+}
+
+window.ctrl = new ViewController()
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const BabiliPlugin = require('babili-webpack-plugin');
+
+const webpackConfig = {
+  mode: 'development', // or 'production'
+  entry: './app/main.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        },
+        include: [path.resolve(__dirname, '../app')]
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 100000
+        }
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      }
+    ]
+  }
+};
+
+if (process.env.NODE_ENV === 'production') {
+  webpackConfig.plugins = [new BabiliPlugin({})];
+  webpackConfig.devtool = 'source-map';
+} else {
+  webpackConfig.devtool = 'eval-source-map';
+}
+
+module.exports = webpackConfig;
